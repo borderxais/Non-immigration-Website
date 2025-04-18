fefbb6b2c6318cfcb947faae252c03d65c56c674
NancyJiangN <nancy@borderxai.com>
2025-04-18 10:31:33 -0700

    1

--- frontend/src/components/DS160Form/sections/PersonalInfoI.tsx
+++ frontend/src/components/DS160Form/sections/PersonalInfoI.tsx
@@ -5,7 +5,7 @@ import DateInput from '../common/DateInput';
 import RepeatableFormItem from '../common/RepeatableFormItem';
 import { countryOptions } from '../utils/formOptions';
 import { FormListFieldData } from 'antd/lib/form/FormList';
-
+import '../ds160Form.css';  // Add this import
 interface PersonalInfoIProps {
   form: any;
 }
@@ -95,17 +95,6 @@ const PersonalInfoI: React.FC<PersonalInfoIProps> = ({ form }) => {
         </div>
       </fieldset>
 
-      <QuestionItem
-        question="您是否曾使用其他姓名？（包括曾用名、英文名、别名等）"
-        name="hasOtherNames"
-        explanation="其它姓名包括您的婚前用名, 宗教用名、职业用名; 或任何为人所知的其它名字；或在过去为别人所知的其它名字。"
-      >
-        <Radio.Group onChange={handleOtherNamesChange}>
-          <Radio value={true}>是</Radio>
-          <Radio value={false}>否</Radio>
-        </Radio.Group>
-      </QuestionItem>
-
       <fieldset className="question-section">
         <div className="section-content">
           <div className="questions-column">
@@ -163,115 +152,152 @@ const PersonalInfoI: React.FC<PersonalInfoIProps> = ({ form }) => {
           </div>
         </div>
       </fieldset>
-
-      <QuestionItem
-        question="您是否有代表您姓名的电码？"
-        name="hasTelecode"
-        explanation="电码由4位数字组成，代表着一些非罗马字母拼写而成的名字的字体。"
-      >
-        <Radio.Group onChange={handleTelecodeChange}>
-          <Radio value={true}>是</Radio>
-          <Radio value={false}>否</Radio>
-        </Radio.Group>
-      </QuestionItem>
       
-      {hasTelecode && (
-        <div className="field-group callout" style={highlightedBlockStyle}>
-          <h4 style={{ marginBottom: '16px', fontWeight: 'normal' }}>请提供以下信息：</h4>
-          
-          <div style={{ maxHeight: '400px', overflowY: 'auto', border: '1px solid #f0f0f0', borderRadius: '4px', padding: '16px' }}>
-            <Form.Item
-              name={['telecode', 'surname']}
-              label="姓氏的电码"
-              rules={[{ required: true, message: '请输入姓氏的电码' }]}
-              style={{ marginBottom: '16px' }}
+      <fieldset className="question-section">
+        <div className="section-content">
+          <div className="questions-column">
+            <QuestionItem
+              question="您是否有代表您姓名的电码？"
+              name="hasTelecode"
             >
-              <Input style={{ width: '95%' }} maxLength={20} />
-            </Form.Item>
+              <Radio.Group onChange={handleTelecodeChange}>
+                <Radio value={true}>是</Radio>
+                <Radio value={false}>否</Radio>
+              </Radio.Group>
+            </QuestionItem>
             
-            <Form.Item
-              name={['telecode', 'givenName']}
-              label="名字的电码"
-              rules={[{ required: true, message: '请输入名字的电码' }]}
-            >
-              <Input style={{ width: '95%' }} maxLength={20} />
-            </Form.Item>
+            {hasTelecode && (
+              <div className="field-group" style={highlightedBlockStyle}>
+                <h4 style={{ marginBottom: '16px', fontWeight: 'normal' }}>请提供以下信息：</h4>
+                
+                <Form.Item
+                  name={['telecode', 'surname']}
+                  label="姓氏的电码"
+                  rules={[{ required: true, message: '请输入姓氏的电码' }]}
+                  style={{ marginBottom: '16px' }}
+                >
+                  <Input style={{ width: '95%' }} maxLength={20} />
+                </Form.Item>
+                
+                <Form.Item
+                  name={['telecode', 'givenName']}
+                  label="名字的电码"
+                  rules={[{ required: true, message: '请输入名字的电码' }]}
+                >
+                  <Input style={{ width: '95%' }} maxLength={20} />
+                </Form.Item>
+              </div>
+            )}
+          </div>
+          
+          <div className="explanations-column">
+            <div className="explanation-item">
+              <h4 style={{ color: '#891300' }}>帮助：电码</h4>
+              <p>电码由4位数字组成，代表着一些非罗马字母拼写而成的名字的字体。</p>
+            </div>
           </div>
         </div>
-      )}
-      
-      <Divider />
-      
-      <QuestionItem
-        question="性别"
-        name="gender"
-        explanation="请选择您的性别，必须与护照上的信息一致。"
-      >
-        <Select placeholder="选择一个">
-          <Select.Option value="M">男</Select.Option>
-          <Select.Option value="F">女</Select.Option>
-        </Select>
-      </QuestionItem>
+      </fieldset>
+      <fieldset className="question-section">
+        <div className="section-content">
+          <div className="questions-column">
+            <QuestionItem
+              question="性别"
+              name="gender"
+            >
+              <Select placeholder="选择一个" style={{ width: '98%' }}>
+                <Select.Option value="M">男</Select.Option>
+                <Select.Option value="F">女</Select.Option>
+              </Select>
+            </QuestionItem>
 
-      <QuestionItem
-        question="婚姻状况"
-        name="maritalStatus"
-        explanation="请选择您目前的婚姻状况。"
-      >
-        <Select placeholder="选择一个">
-          <Select.Option value="M">已婚</Select.Option>
-          <Select.Option value="C">普通法婚姻</Select.Option>
-          <Select.Option value="P">民事结合/同居伴侣关系</Select.Option>
-          <Select.Option value="S">未婚</Select.Option>
-          <Select.Option value="W">丧偶</Select.Option>
-          <Select.Option value="D">离异</Select.Option>
-          <Select.Option value="L">合法分居</Select.Option>
-          <Select.Option value="O">其他</Select.Option>
-        </Select>
-      </QuestionItem>
-      
-      <Divider />
+            <QuestionItem
+              question="婚姻状况"
+              name="maritalStatus"
+            >
+              <Select placeholder="选择一个" style={{ width: '98%' }}>
+                <Select.Option value="M">已婚</Select.Option>
+                <Select.Option value="C">普通法婚姻</Select.Option>
+                <Select.Option value="P">民事结合/同居伴侣关系</Select.Option>
+                <Select.Option value="S">未婚</Select.Option>
+                <Select.Option value="W">丧偶</Select.Option>
+                <Select.Option value="D">离异</Select.Option>
+                <Select.Option value="L">合法分居</Select.Option>
+                <Select.Option value="O">其他</Select.Option>
+              </Select>
+            </QuestionItem>
+          </div>
+          
+          <div className="explanations-column">
+            <div className="explanation-item">
+              <h4 style={{ color: '#891300' }}>帮助：性别</h4>
+              <p>请选择您的性别，必须与护照上的信息一致。</p>
+            </div>
+            
+            <div className="explanation-item">
+              <h4 style={{ color: '#891300' }}>帮助：婚姻状况</h4>
+              <p>请选择您目前的婚姻状况。</p>
+            </div>
+          </div>
+        </div>
+      </fieldset>
 
-      <h4 style={{ marginBottom: '10px', marginTop: '20px' }}>
-        <span>出生日期与出生地</span>
-      </h4>
-    
-      <div className="field-group callout" style={highlightedBlockStyle}>
-        <QuestionItem
-          question="日期"
-          explanation="若不知道具体日期或月份，请按护照所示填写。"
-        >
-          <DateInput 
-            dayName="dobDay" 
-            monthName="dobMonth" 
-            yearName="dobYear"
-          />
-        </QuestionItem>
+      <fieldset className="question-section">
+        <div className="section-content">
+          <div className="questions-column">
+            <h4 style={{ marginBottom: '10px' }}>
+              <span>出生日期与出生地</span>
+            </h4>
+            
+            <div className="field-group" style={highlightedBlockStyle}>
+              <QuestionItem
+                question="日期"
+              >
+                <DateInput 
+                  dayName="dobDay" 
+                  monthName="dobMonth" 
+                  yearName="dobYear"
+                />
+              </QuestionItem>
 
-        <QuestionItem
-          question="城市"
-          name="birthPlace"
-        >
-          <Input placeholder="例如：北京" />
-        </QuestionItem>
+              <QuestionItem
+                question="城市"
+                name="birthPlace"
+              >
+                <Input placeholder="例如：北京" style={{ width: '99%' }} />
+              </QuestionItem>
 
-        <QuestionItem
-          question="州/省"
-          name="birthState"
-          hasNaCheckbox={true}
-          naCheckboxName="birthState_na"
-        >
-          <Input placeholder="例如：北京市" />
-        </QuestionItem>
+              <QuestionItem
+                question="州/省"
+                name="birthState"
+                hasNaCheckbox={true}
+                naCheckboxName="birthState_na"
+              >
+                <Input placeholder="例如：北京市" style={{ width: '99%' }} />
+              </QuestionItem>
 
-        <QuestionItem
-          question="国家/地区"
-          name="birthCountry"
-          explanation="请选择您出生地的现用国家/地区名称。"
-        >
-          <Select options={countryOptions} placeholder="- 选择一个 -" />
-        </QuestionItem>
-      </div>
+              <QuestionItem
+                question="国家/地区"
+                name="birthCountry"
+              >
+                <Select options={countryOptions} placeholder="- 选择一个 -" style={{ width: '98%' }} />
+              </QuestionItem>
+            </div>
+          </div>
+          
+          <div className="explanations-column">
+            <div className="explanation-item">
+              <h4 style={{ color: '#891300' }}>帮助：出生日期</h4>
+              <p>若不知道具体日期或月份，请按护照所示填写。</p>
+            </div>
+            
+            <div className="explanation-item">
+              <h4 style={{ color: '#891300' }}>帮助：出生地国家/地区</h4>
+              <p>请选择您出生地的现用国家/地区名称。</p>
+            </div>
+          </div>
+        </div>
+      </fieldset>
     </div>
   );
 };
